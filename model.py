b2324a7bb12cfadde03aef7eeef7db0129613bce
Charles Ma <charlesma@unc.edu>
2020-12-04 16:04:02 -0500

    Init Setup

--- model.py
+++ model.py
@@ -1,3 +1,7 @@
 import numpy as np
 import scipy.stats as si
 import sympy as sy
+
+def euro_call(S, K, T, r, sigma):
+    d1 = (np.log(S / K) + (r + 0.5 * sigma ** 2) * T) / (sigma * np.sqrt(T))
+    return d1
